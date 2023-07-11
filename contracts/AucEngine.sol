1e7178760754adfef6038ad7a47a253fbf9c14da
knQzx <75641500+knQzx@users.noreply.github.com>
2023-07-12 00:08:34 +0300

    Update AucEngine.sol
    made:
    - owner
    - massive of offers
    - Auction struct

--- contracts/AucEngine.sol
+++ contracts/AucEngine.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract AucEngine {
+    address public owner;
+    uint constant DURATION = 2 days;
+    uint constant FEE = 10; // 10%
+
+
+    struct Auction {
+        address payable seller;
+        uint startingPrice;
+        uint finalPrice;
+        uint startAt;
+        uint endsAt;
+        uint discountRate;
+        string item;
+        bool stopped;
+    }
+
+    Auction[] public auctions;
+
+    constructor() {
+        owner = msg.sender;
+    } 
+}
